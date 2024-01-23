0525749569e6b611b9593f7a317f45c9bb41980e
Arsene SOUMEU <soumeua@wwfcam.org>
2024-01-23 15:09:39 +0100

    version 1 pillier communication OK

---
+++ app.py
@@ -0,0 +1,51 @@
+#######################
+# Import libraries
+import streamlit as st
+import pandas as pd
+import altair as alt
+import plotly.express as px
+# import pandas_profiling
+# from streamlit_pandas_profiling import st_profile_report
+import time
+import requests
+# from pydantic_settings import BaseSettings 
+import datetime
+from streamlit_option_menu import option_menu
+#######################
+# Page configuration
+from views.communication import *
+from views.wildlife import wildlife
+
+st.set_page_config(
+    page_title="Congo Basin Monitoring and Evaluation database",
+    # page_icon="🏂",
+    layout="wide",
+    initial_sidebar_state="expanded")
+
+alt.themes.enable("dark")
+
+
+pillars= [
+    "communication","wildlife","human activity","law enforcement","spatial data and landcover","capacity building"
+]
+with st.sidebar:
+    # st.title('🏂 US Population Dashboard')
+    st.markdown(
+            '<img src="./app/static/logo.jpg" height="120" style="">',
+            unsafe_allow_html=True,
+        )
+    # st.image("static/logo.jpg")
+    st.title('Congo Basin Monitoring and Evaluation Database')
+    
+    # indicator_list = list(df_reshaped.year.unique())[::-1]
+    
+    selected_pillar = st.selectbox('Select Pillar', pillars)
+    
+    
+content = st.empty()
+if selected_pillar =="communication":
+    communication_page(st)
+elif selected_pillar =="wildlife":
+    wildlife()
+
+    
---
+++ data/load_data.py
@@ -0,0 +1,65 @@
+
+
+import time
+import requests
+import streamlit as st
+
+
+@st.cache_data
+def load_data(url_dict,st):
+    url_login ="https://biomonitoringwebsite.herokuapp.com/api/auth/login"
+    data = {
+
+        "username": "biomonitoring",
+        "password": "wwfcam"
+
+    }
+
+    x = ''
+    while x == '':
+        try:
+            x = requests.post(url_login, json=data)
+            break
+        except:
+
+            time.sleep(5)
+
+            continue
+
+    # print(x.json())
+    x = x.json()
+    token = x["token"]
+    # localurl = "http://localhost:8000"
+    # onlineurl = "https://biomonitoringwebsite.herokuapp.com"
+    # dataurl =onlineurl+"/api/communication/"
+    # datapub_typeurl =localurl+"/api/publicationType/"
+    data_dict = {}
+    # datapub_type = ''
+    data=''
+    progress_text = "Data loading. Please wait."
+    with st.spinner(progress_text):
+        while data == '':
+            try:
+                # blocks = requests.get(urlblock, headers = {"Authorization":  'Token '+str(token)})
+                # sectors = requests.get(urlsectors, headers = {"Authorization":  'Token '+str(token)})
+                # sites = requests.get(urlsites, headers = {"Authorization":  'Token '+str(token)})
+                # sublandscapes = requests.get(urllandscapes, headers = {"Authorization":  'Token '+str(token)})
+                # landscapes = requests.get(urlmain_Landscapes, headers = {"Authorization":  'Token '+str(token)})
+                # species = requests.get(urlspecies, headers = {"Authorization":  'Token '+str(token)})
+                # samplingMethod = requests.get(urlsamplingMethods, headers = {"Authorization":  'Token '+str(token)})
+                for key, value in url_dict.items():
+                    # st.write(key + '=' + value)
+                    data_dict[key] = requests.get(value, headers = {"Authorization":  'Token '+str(token)})
+                data = 'ok'
+                # datapub_type = requests.get(datapub_typeurl, headers = {"Authorization":  'Token '+str(token)})
+                break
+            except:
+
+                time.sleep(5)
+
+                continue
+    # st.write(data_dict)
+    data_dict = {key:value.json() for key,value in data_dict.items()}
+    # st.toast('#### data loaded!', icon='🎉')
+    st.success('Data loaded!', icon="✅")
+    return data_dict
