f079c562e06b77dec4f1fcc50f8d5b5026a235bb
Arsene SOUMEU <soumeua@wwfcam.org>
2024-02-05 09:40:53 +0100

    Dashboard region level general information

--- views/wildlifedata/wildlife_region.py
+++ views/wildlifedata/wildlife_region.py
@@ -7,52 +7,7 @@ import geopandas as gpd
 import altair as alt
 import folium
 from streamlit_folium import st_folium
-st.markdown("""
-    <style>
 
-    [data-testid="block-container"] {
-        padding-left: 2rem;
-        padding-right: 2rem;
-        padding-top: 1rem;
-        padding-bottom: 0rem;
-        margin-bottom: -7rem;
-    }
-
-    [data-testid="stVerticalBlock"] {
-        padding-left: 0rem;
-        padding-right: 0rem;
-    }
-
-    [data-testid="stMetric"] {
-        background-color: #393939;
-        text-align: center;
-        padding: 15px 0;
-    }
-
-    [data-testid="stMetricLabel"] {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    }
-
-    [data-testid="stMetricDeltaIcon-Up"] {
-        position: relative;
-        left: 38%;
-        -webkit-transform: translateX(-50%);
-        -ms-transform: translateX(-50%);
-        transform: translateX(-50%);
-    }
-
-    [data-testid="stMetricDeltaIcon-Down"] {
-        position: relative;
-        left: 38%;
-        -webkit-transform: translateX(-50%);
-        -ms-transform: translateX(-50%);
-        transform: translateX(-50%);
-    }
-
-    </style>
-    """, unsafe_allow_html=True)
 # @st.cache_data(experimental_allow_widgets=True)
 def wildlife_region(st,data,pd):
     st.header("Congo Basin wildlfe dashboard")
