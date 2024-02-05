768b9eee7dff8a2a210a1281f2a06b2096925524
Arsene SOUMEU <soumeua@wwfcam.org>
2024-02-05 08:43:04 +0100

    Dashboard region level general information

--- views/wildlifedata/wildlife_region.py
+++ views/wildlifedata/wildlife_region.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 from data.load_data import *
-from tools import calculate_lenght_difference,format_number, naturalbreaksMap
+from tools import *
 from streamlit_option_menu import option_menu
 import geopandas as gpd
 import altair as alt
@@ -17,20 +17,44 @@ def wildlife_region(st,data,pd):
     max_year = df['year'].max()
     sitesgdf = gpd.read_file("data/All_site.shp",)
     sitesgdf["site"] = sitesgdf[sitesgdf["Level"]=="Site"]["Site_1"]
+    sampling_methods  = data["sampling_method"]
+    sampling_method_ids  = df["sampling_method"].unique()
+    sampling_methods = sampling_methods.loc[sampling_methods["id"].isin(sampling_method_ids)][["name","id"]].drop_duplicates()
+    # st.write(sampling_methods)
+    sampling_name_id = {x.replace("_"," "):sampling_methods.loc[sampling_methods["name"]==x]["id"].unique()[0] for x in sampling_methods["name"].unique() if x != None} 
+    sampling_method_names = ["All"]+[key for key,value in sampling_name_id.items() ]
     with st.sidebar:
     # st.title('🏂 US Population Dashboard')
     # st.title('Filter')
+        select_sampling_method = st.selectbox(
+            'Select sampling method',
+            options=sampling_method_names
+        )
         start_year, end_year = st.select_slider(
             'Select year range',
             options=years,
             value=(min_year, max_year)
         )
+        
     df = df.loc[(df["year"]>=start_year)&(df["year"]<=end_year)]
-    df["year"] = df["year"].astype(str)
+    if select_sampling_method != "All":
+        # st.write(sampling_name_id[select_sampling_method])
+        df = df.loc[df["sampling_method"]==sampling_name_id[select_sampling_method]]
+    
+
     landscapesdf  = data["landscapes"]
     sitesdf  = data["sites"]
     countriesdf  = data["countries"]
     speciesdf  = data["species"]
+    df["year"] = df["year"].astype(str)
+    df["main_landscape"] = df["main_landscape"].astype(str)
+    area_cover_df =  get_area_covered_table(df ,sitesdf)
+    # print("TRIDOM GAB",get_max_area_covered_per_level(area_cover_df,1,"Country"))
+    # st.write(area_cover_df)
+    # data = get_cumulative_max_area_covered_per_level_per_year(area_cover_df,1,"Region","area_covered")
+    # print("Cumulative TRIDOM GAB",data)
+    # cumulativedf = get_cumulative_max_area_covered_per_level_per_year_table(area_cover_df,"Site")
+   
     species_field_mask = ["name","sites","sites_number", "priority"]
     species_result_df = getspeciesByLevel(df,speciesdf,sitesdf,"region","species","site",1).sort_values(by=['sites_number'], ascending=[ False])
     sites_result_df = getspeciesByLevel(df,sitesdf,speciesdf,"region","site","species",1).sort_values(by=['sites_number'], ascending=[ False])
